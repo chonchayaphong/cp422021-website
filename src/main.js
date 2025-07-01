593aa5ea5b7262864e63f4a86ddcbf27b74962f7
Chonchayaphong <chonchayaphong.c@kkumail.com>
2025-07-01 19:22:52 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
 
 // Styles
+
 import '@core/scss/template/index.scss'
 import '@layouts/styles/index.scss'
 
 // Create vue app
+
 const app = createApp(App)
 
 
