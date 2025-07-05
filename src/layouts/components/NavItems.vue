69e5e1a96930da7bd4f0cccfa472c4b3175a6cab
Chonchayaphong <chonchayaphong.c@kkumail.com>
2025-07-05 20:18:51 +0700

    update src

--- src/layouts/components/NavItems.vue
+++ src/layouts/components/NavItems.vue
@@ -1,200 +1,96 @@
 <script setup>
-import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
-import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue'
-import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue';
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue';
 </script>
 
 <template>
-  <!-- 👉 Dashboards -->
-  <VerticalNavGroup
-    :item="{
-      title: 'Dashboards',
-      badgeContent: '5',
-      badgeClass: 'bg-error',
-      icon: 'ri-home-smile-line',
-    }"
-  >
-    <VerticalNavLink
-      :item="{
-        title: 'Analytics',
-        to: '/dashboard',
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'CRM',
-        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/dashboards/crm',
-        target: '_blank',
-        badgeContent: 'Pro',
-        badgeClass: 'bg-light-primary text-primary',
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'ECommerce',
-        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/dashboards/ecommerce',
-        target: '_blank',
-        badgeContent: 'Pro',
-        badgeClass: 'bg-light-primary text-primary',
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Academy',
-        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/dashboards/academy',
-        target: '_blank',
-        badgeContent: 'Pro',
-        badgeClass: 'bg-light-primary text-primary',
-      }"
-    />
-    <VerticalNavLink
-      :item="{
-        title: 'Logistics',
-        href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/dashboards/logistics',
-        target: '_blank',
-        badgeContent: 'Pro',
-        badgeClass: 'bg-light-primary text-primary',
-      }"
-    />
-  </VerticalNavGroup>
-
-
-
-
   
+
+  <!-- 👉 Point of Sale -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Point of Sale',
+    }"
+  />
+
+  <VerticalNavLink
+    :item="{
+      title: 'โต๊ะอาหาร',
+      icon: 'mdi-table-chair',
+      to: '/table',
+    }"
+  />
+
+  <VerticalNavLink
+    :item="{
+      title: 'จองคิว',
+      icon: 'mdi-human-queue',
+      to: '/queue',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'เมนูอาหาร',
+      icon: 'mdi-menu',
+      to: '/menu',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'สต็อกสินค้า',
+      icon: 'mdi-store-check',
+      to: '/stock'
+    }"
+  />
+
  
-  
- 
-
-  <VerticalNavLink
-    :item="{
-      title: 'Account Settings',
-      icon: 'ri-user-settings-line',
-      to: '/account-settings',
-    }"
-  />
-
-  <VerticalNavLink
-    :item="{
-      title: 'Login',
-      icon: 'ri-login-box-line',
-      to: '/login',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Register',
-      icon: 'ri-user-add-line',
-      to: '/register',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Error',
-      icon: 'ri-information-line',
-      to: '/no-existence',
-    }"
-  />
-
-  <!-- 👉 User Interface -->
   <VerticalNavSectionTitle
     :item="{
-      heading: 'User Interface',
+      heading: 'การเงิน',
     }"
   />
   <VerticalNavLink
     :item="{
-      title: 'Typography',
-      icon: 'ri-text',
-      to: '/typography',
+      title: 'รายรับ',
+      icon: 'mdi-currency-usd',
+      to: '/income',
     }"
   />
   <VerticalNavLink
     :item="{
-      title: 'Icons',
-      icon: 'ri-remixicon-line',
-      to: '/icons',
+      title: 'รายจ่าย',
+      icon: 'mdi-cash-clock',
+      to: '/expend',
     }"
   />
   <VerticalNavLink
     :item="{
-      title: 'Cards',
-      icon: 'ri-bar-chart-box-line',
-      to: '/cards',
+      title: 'ธุรกรรมย้อนหลัง',
+      icon: 'mdi-sd',
+      to: '/transaction',
     }"
   />
 
-  <!-- 👉 Forms & Tables -->
+
   <VerticalNavSectionTitle
     :item="{
-      heading: 'Forms & Tables',
+      heading: 'ผู้ดูแลระบบ',
     }"
   />
   <VerticalNavLink
     :item="{
-      title: 'Form Layouts',
-      icon: 'ri-layout-4-line',
-      to: '/form-layouts',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Form Validation',
-      icon: 'ri-checkbox-multiple-line',
-      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/forms/form-validation',
-      target: '_blank',
-      badgeContent: 'Pro',
-      badgeClass: 'bg-light-primary text-primary',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Form Wizard',
-      icon: 'ri-git-commit-line',
-      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/forms/form-wizard-numbered',
-      target: '_blank',
-      badgeContent: 'Pro',
-      badgeClass: 'bg-light-primary text-primary',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Tables',
-      icon: 'ri-table-alt-line',
-      to: '/tables',
+      title: 'ผู้ใช้งาน',
+      icon: 'mdi-account',
+      to: '/user',
     }"
   />
 
-  <!-- 👉 Others -->
-  <VerticalNavSectionTitle
-    :item="{
-      heading: 'Others',
-    }"
-  />
   <VerticalNavLink
     :item="{
-      title: 'Access Control',
-      icon: 'ri-shield-line',
-      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/demo-1/access-control',
-      target: '_blank',
-      badgeContent: 'Pro',
-      badgeClass: 'bg-light-primary text-primary',
+      title: 'ตั้งค่าระบบ',
+      icon: 'mdi-cogs',
+      to: '/setting',
     }"
   />
-  <VerticalNavLink
-    :item="{
-      title: 'Documentation',
-      icon: 'ri-article-line',
-      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/documentation/',
-      target: '_blank',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Raise Support',
-      href: 'https://github.com/themeselection/materio-vuetify-vuejs-admin-template-free/issues',
-      icon: 'ri-lifebuoy-line',
-      target: '_blank',
-    }"
-  />
+
 </template>
